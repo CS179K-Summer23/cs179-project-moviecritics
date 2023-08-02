57b9c387ae9f54ccbbb544ffeec4a361ba012dd1
Harsh Patel <hp6164@hotmail.com>
2023-08-02 14:41:55 -0700

    Ready for front end dev

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
-import App from "./app";
+import App from "./app.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
