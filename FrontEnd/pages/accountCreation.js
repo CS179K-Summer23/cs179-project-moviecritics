f29426ad17011e8622ac1b0b6eed1772b3a36a83
Harsh Patel <hp6164@hotmail.com>
2023-08-04 15:32:06 -0700

    Pagebasic view (creation not successful)

--- FrontEnd/pages/accountCreation.js
+++ FrontEnd/pages/accountCreation.js
@@ -4,29 +4,39 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Button, FormGroup, FormControlLabel, Checkbox, Dialog, DialogContent, DialogTitle, CssBaseline, FormControl, InputLabel, Select, MenuItem, Box, TextField } from "@mui/material";
 
 export default function AccountCreation() {
-    return (
+    
+  const CreateAccountAction = (event) => {
+    console.log({
+      //email: document.getElementById ("emailbox").value,
+      //password:document.getElementById ("passwordbox").value
+    });
+  };
+  
+  return (
       <>
+          <FormGroup>
           <Box sx={{
               marginTop: 8,
               display: 'flex',
               flexDirection: 'column',
-              alignItems: 'center', }} >
-
-           <TextField margin="normal" required id="namebox" label="Name" autoComplete="name" />
+              alignItems: 'center',
+              backgroundColor: 'lightblue',
+              width:'500px',
+              align:'center', onSubmit:{CreateAccountAction} }} >
+          
+            <TextField margin="normal" required id="namebox" label="Name" autoComplete="name" />
           <p></p>
-           <TextField required id="outlined-required" label="Required" defaultValue="Age" />
+          <TextField margin="normal" required id="agebox" label="Age" autoComplete="Age" />
            <p></p>
-           <TextField required id="outlined-required" label="Required" defaultValue="Email" />
+           <TextField margin="normal" required id="emailbox" label="Email" autoComplete="Email" />
            <p></p>
-           <TextField required id="outlined-required" label="Required" defaultValue="Password" />
+           <TextField margin="normal" required id="passwordbox" label="Password" autoComplete="Password" />
            <p></p>
-           <TextField required id="outlined-required" label="Required" defaultValue="Re-Enter Password"/>
+           <TextField margin="normal" required id="repasswordbox" label="Re-Enter-Password" autoComplete="Re-Enter-Password" />
            <p></p>
-          <Button variant="contained">Create Account</Button>
-
-
+          <Button variant="contained" type="submit" onClick={CreateAccountAction} >Create Account</Button>
           </Box>
-           
+          </FormGroup>
       </>
     );
   }
