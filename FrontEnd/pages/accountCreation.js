2754bfb36f592c3a8162f09eef97dc655f725a86
hp6164 <hp6164@hotmail.com>
2023-08-05 23:07:09 -0700

    Added a Dark theme, and updated creation-page

--- FrontEnd/pages/accountCreation.js
+++ FrontEnd/pages/accountCreation.js
@@ -1,42 +1,62 @@
 import * as React from "react";
 import { useState } from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { Button, FormGroup, FormControlLabel, Checkbox, Dialog, DialogContent, DialogTitle, CssBaseline, FormControl, InputLabel, Select, MenuItem, Box, TextField } from "@mui/material";
+import { Button, Box, TextField } from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function AccountCreation() {
+
+  const handleChange = (event) => {
     
-  const CreateAccountAction = (event) => {
-    console.log({
-      //email: document.getElementById ("emailbox").value,
-      //password:document.getElementById ("passwordbox").value
-    });
   };
-  
+
+  const CreateAccountAction = async (event) => {
+    event.preventDefault();
+
+  };
+
   return (
-      <>
-          <FormGroup>
-          <Box sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              backgroundColor: 'lightblue',
-              width:'500px',
-              align:'center', onSubmit:{CreateAccountAction} }} >
-          
-            <TextField margin="normal" required id="namebox" label="Name" autoComplete="name" />
+    <>
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          align:'center',
+          width: '400px',
+          borderRadius: 8,
+          marginLeft:'auto',
+          marginRight:'auto',
+        }}
+        
+      >
+        <form onSubmit={CreateAccountAction}>
+          <h1>Create Your Account</h1>
           <p></p>
-          <TextField margin="normal" required id="agebox" label="Age" autoComplete="Age" />
-           <p></p>
-           <TextField margin="normal" required id="emailbox" label="Email" autoComplete="Email" />
-           <p></p>
-           <TextField margin="normal" required id="passwordbox" label="Password" autoComplete="Password" />
-           <p></p>
-           <TextField margin="normal" required id="repasswordbox" label="Re-Enter-Password" autoComplete="Re-Enter-Password" />
-           <p></p>
-          <Button variant="contained" type="submit" onClick={CreateAccountAction} >Create Account</Button>
-          </Box>
-          </FormGroup>
-      </>
-    );
-  }
+          <TextField id="name" label="Name" required onChange={handleChange} />
+          <p></p>
+          <TextField id="age" label="Age" required onChange={handleChange} />
+          <p></p>
+          <TextField id="email" label="Email" required onChange={handleChange} />
+          <p></p>
+          <TextField id="password" label="Password" type="password" required onChange={handleChange} />
+          <p></p>
+          <TextField id="repassword" label="Re-Enter-Password" type="password" required onChange={handleChange} />
+          <p></p>
+          <Button variant="contained" type="submit">Create Account</Button>
+          <p></p>
+        </form>
+      </Box>
+      </ThemeProvider>
+    </>
+  );
+}
