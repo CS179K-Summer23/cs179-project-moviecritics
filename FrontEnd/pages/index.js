0f85f32b1d791504307a5aafa1f7f01ac52cde34
manojn29 <mnaga024@ucr.edu>
2023-08-06 19:34:02 -0700

    Page transition changes.

--- FrontEnd/pages/index.js
+++ FrontEnd/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, {useState} from "react";
 import * as ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./app.js";
@@ -8,18 +8,29 @@ import AccountCreation from "./accountCreation.js";
 import UserSurveyApp from "./UserSurvey.js";
 
 export default function Home() {
+  const [accountCreated, setAccountCreated] = useState(false);
+
+  const handleAccountCreationSuccess = () => {
+    setAccountCreated(true);
+  };
   return (
     <>
         <title>MovieCritics</title>
         <meta name="description" content="Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="" />
-        <AccountCreation />
-        <UserSurveyApp />
-        <MovieshowerFromInterests />
-        <MovieRating />
+        {/* <AccountCreation /> */}
+        {/* If account isn't created, display accountCreation */}
+        {!accountCreated && <AccountCreation onSuccess={handleAccountCreationSuccess} />}
         
-     
+        {/* If Account is created, show UserSurvey */}
+        {accountCreated && (
+          <>
+            <UserSurveyApp />
+          </>
+        )}
+        {/* <MovieshowerFromInterests />
+        <MovieRating /> */}
     </>
   );
 }
