9ffffdabf10e24267d2e942dfdc109fe80f01dbd
Patelriyaaa <91497158+Patelriyaaa@users.noreply.github.com>
2023-08-06 17:40:41 -0700

    Started MovieRating

--- FrontEnd/pages/index.js
+++ FrontEnd/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./app.js";
-import MovieshowerFromInterests from "./interestsFromSurvey.js";
+import MovieRating from "./MovieRating.js";
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="description" content="Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="" />
-        <MovieshowerFromInterests />
+        <MovieRating />
         
     </>
   );
