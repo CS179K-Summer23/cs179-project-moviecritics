055c12107d09110b14c7a44ec1ca692abdea907c
Sev <39773925+CC-Sev@users.noreply.github.com>
2023-08-13 19:19:32 -0700

    Update sort_genre_rank.py

--- BackEnd/sort_genre_rank.py
+++ BackEnd/sort_genre_rank.py
@@ -3,18 +3,20 @@ import json
 
 csv_file = 'movies_db.csv'
 
-def top25_by_genre(csv_file, target_genres, min_vote_count=1000, limit=25):
+def top25_by_genre(csv_file, target_genres, min_vote_count=1000, limit=25, age=None):
     movies_list = []
-    with open(csv_file, 'r', newline='', encoding='utf-8') as file:
+    with open(csv_file, 'r', newline='') as file:
         reader = csv.DictReader(file)
-        print("this is reader")
-        print(reader)
         for row in reader:
             movie_title = row['title']
             movie_genres = row['genres'].split('-')
             vote_count = float(row['vote_count'])
             vote_average = float(row['vote_average'])
             movie_rating = row['Rated']
+            
+            if age is not None and age < 13 and movie_rating == 'PG-13':
+                continue
+            
             if any(genre.strip().lower() in target_genres for genre in movie_genres) and vote_count > min_vote_count:
                 movie_info = {
                     'title': movie_title,
@@ -25,22 +27,23 @@ def top25_by_genre(csv_file, target_genres, min_vote_count=1000, limit=25):
                 }
                 movies_list.append(movie_info)
         
-        # Sort
         sorted_movies = sorted(movies_list, key=lambda x: x['vote_average'], reverse=True)
         
         return sorted_movies[:limit]
 
 if __name__ == "__main__":
-    user_genres = input("Enter genres (Seperate gnenres by commas): ")
+    user_genres = input("Enter genres (Separate genres by commas): ")
     user_genres = [genre.strip().lower() for genre in user_genres.split(",")]
-    print(user_genres)
+    
+    user_age = int(input("Enter your age: "))
+    
     if 1 <= len(user_genres) <= 5:
-        movies_data = top25_by_genre(csv_file, user_genres)
-        #print(movies_data)
+        movies_data = top25_by_genre(csv_file, user_genres, age=user_age)
+        
         if movies_data:
             json_data = json.dumps(movies_data, indent=2)
             print(json_data)
         else:
-            print(f"No movies found :( ")
+            print("No movies found :(")
     else:
         print("Please enter 1 to 5 genres separated by commas.")
