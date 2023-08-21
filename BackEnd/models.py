2869c7ec155f00ec50ec1d5b2e8c4e333a56af1c
hp6164 <hp6164@hotmail.com>
2023-08-21 11:12:00 -0700

    adding model

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -8,3 +8,10 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+
+class User_watchlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    movie_id = db.Column(db.String(120), unique=True, nullable=False)
