b0c08079ea68724d1ae83f9fea7d55fd512e81a1
hp6164 <hp6164@hotmail.com>
2023-08-21 01:52:21 -0700

    new class in models

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -8,3 +8,14 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+
+
+class UserWatchlist(db.Model):
+    tablename = 'user_watchlist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    movie_id = db.Column(db.ARRAY(db.Integer))
+
+    user = db.relationship('User', backref=db.backref('watchlist', lazy=True))
