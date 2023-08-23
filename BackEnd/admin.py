b96ec6d3180676610a4f6214552eaf93cf75e841
hp6164 <hp6164@hotmail.com>
2023-08-23 16:37:38 -0700

    Adding Basic menu for admin

---
+++ BackEnd/admin.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def main():
+    i=0
+    print('Welcome to the Admin portal')
+    while i != 6:
+        print('1. See Requested Movies')
+        print('2. View Movie Database')
+        print('3. Add a movie to Database')
+        print('4. Delete a movie in Database')
+        print('5. Update movie in Database')
+        print('6. EXIT')
+        i = input("Enter choice:")
+        if (i )
+
+
+
+  
+  
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
